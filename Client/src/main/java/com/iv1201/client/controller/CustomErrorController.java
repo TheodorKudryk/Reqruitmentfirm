11a4fb81ab4082868e861aea22c5b582e4ec2850
kudryk <theo.kudryk@gmail.com>
2022-03-11 17:17:38 +0100

    ,

--- Client/src/main/java/com/iv1201/client/controller/CustomErrorController.java
+++ Client/src/main/java/com/iv1201/client/controller/CustomErrorController.java
@@ -2,6 +2,7 @@
 package com.iv1201.Client.controller;
 
 import static com.iv1201.client.controller.LoginController.isAuthenticated;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
@@ -17,11 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 @AutoConfigureAfter(CustomErrorController.class)
 public class CustomErrorController implements ErrorController  {
 
+    
     public CustomErrorController(){
         System.out.println("Created CustomErrorController");
     }
         @RequestMapping("/error")
-        public String handleError() {
+        public String handleError(HttpServletRequest request) {
             if (isAuthenticated())
                 return "redirect:/startpage?error";
             return "redirect:/login?error";
